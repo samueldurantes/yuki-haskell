b2559b44fdb752baba35264487a72775d98ff8d1
Samuel Durante <samueldurante.h@gmail.com>
2021-09-19 23:13:08 -0300

    Redundant bracket again

--- lib/Yuki/Commands/Ping.hs
+++ lib/Yuki/Commands/Ping.hs
@@ -4,4 +4,4 @@ import Discord.Types
 import qualified Discord.Requests as R
 
 ping :: Message -> R.ChannelRequest Message
-ping m = (R.CreateMessage (messageChannel m) "Pong!")
+ping m = R.CreateMessage (messageChannel m) "Pong!"
